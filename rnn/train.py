87be949e7d6e3f1f2f005923c26fcba4ef6617fd
jf773 <jf773@cornell.edu>
2017-11-14 03:59:04 -0500

    rnn model

---
+++ rnn/train.py
@@ -0,0 +1,78 @@
+import rnn_model
+import argparse
+import numpy as np
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model',type=str, default='lstm',
+                        help='tpye of rnn:lstm, gru or rnn')
+    parser.add_argument('--load_model', type = bool, default = False,
+                        help='whether load a pre-traind model')
+    parser.add_argument('--weights_fpath',type=str, default = 'model/',
+                        help='path of weitght')
+    parser.add_argument('--num_hidden', type=int, default=256,
+                       help='size of RNN hidden state')
+    parser.add_argument('--num_layers', type=int, default=3,
+                       help='number of layers in the RNN')  
+    parser.add_argument('--batch_size', type=int, default=10,
+                       help='minibatch size')
+    parser.add_argument('--train_seq_length', type=int, default=15,
+                       help='RNN sequence length in training phase')
+    parser.add_argument('--max_epochs', type=int, default=100,
+                       help='number of epochs')
+    parser.add_argument('--grad_clipping', type=float, default=1.,
+                       help='clip gradients at this value') 
+    parser.add_argument('--lr', type=float, default=0.002,
+                       help='learning rate')
+    parser.add_argument('--lr_decay', type=float, default=0.97,
+                       help='the decay rate of learning rate')
+    parser.add_argument('--lr_decay_after', type=int, default =10,
+                       help='number of epochs to start decaying the learning rate') 
+    parser.add_argument('--input_train_fpath', type=str, default='data/input_train.csv',
+                       help='data directory containing input_train')
+    parser.add_argument('--output_train_fpath', type=str, default='data/output_train.csv',
+                       help='data directory containing output_train')
+    parser.add_argument('--input_test_fpath', type=str, default='data/input_test.csv',
+                       help='data directory containing input_test')
+    parser.add_argument('--output_test_fpath', type=str, default='data/output_test.csv',
+                       help='data directory containing output_test')
+    args = parser.parse_args()
+    train(args)
+
+def train(args):
+    model = args.model # type of RNN
+    weights_fpath = args.weights_fpath  # weights will be stored here
+    input_train_fpath = args.input_train_fpath
+    output_train_fpath = args.output_train_fpath
+    input_test_fpath = args.input_test_fpath
+    output_test_fpath = args.output_test_fpath
+    max_epochs = args.max_epochs
+    lr = args.lr
+    lr_decay = args.lr_decay
+    lr_decay_after = args.lr_decay_after
+    grad_clipping = args.grad_clipping  
+    num_hidden = args.num_hidden
+    num_layers = args.num_layers
+    batch_size = args.batch_size
+    train_seq_length = args.train_seq_length
+    load_model = args.load_model
+
+    print("rnn: {}".format(model))
+    print("num_hidden: {}".format(num_hidden))
+    print("max_epochs: {}".format(max_epochs))
+
+    print('Start Training')
+
+    g = rnn_model.build_graph(model, lr, lr_decay, lr_decay_after,
+        grad_clipping, num_hidden, num_layers, batch_size, train_seq_length)
+    rnn_model.train_graph(load_model, model, num_layers, num_hidden, g, batch_size,
+        train_seq_length, max_epochs, weights_fpath, input_train_fpath,
+        input_test_fpath, output_train_fpath, output_test_fpath)
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ rnn/utils.py
@@ -0,0 +1,93 @@
+import numpy as np
+import csv
+
+def max_length(input_path):
+	i = max_length = 0
+	input_data = input_parse(input_path)
+	while i < input_data.shape[0]:
+		first = input_data[i, 0]
+		start = i
+		while i < input_data.shape[0] and input_data[i, 0] == first:
+			i += 1
+		end = i
+		max_length = max(max_length, end - start)
+	return max_length
+
+def input_parse(fpath):
+	with open(fpath, 'r') as f:
+		data = list(csv.reader(f, quoting=csv.QUOTE_NONNUMERIC))
+	data = np.asarray(data, dtype=np.float32)
+	return data
+
+def output_parse(fpath):
+	with open(fpath, 'r') as f:
+		data = list(csv.reader(f, quoting=csv.QUOTE_NONNUMERIC))
+	data = np.asarray(data, dtype=np.float32)
+	return data
+# build the batches for training: X[i] --> y[i]
+# ['H' 'e' 'l' 'l' 'o' ' ' 'd' 'a' 'r'] --> 'k'
+# ['e' 'l' 'l' 'o' ' ' 'd' 'a' 'r' 'k'] --> 'n'
+# ['l' 'l' 'o' ' ' 'd' 'a' 'r' 'k' 'n'] --> 'e', and so on
+def sequences(batch_size, sequence_length, input_path, output_path):
+	input_data = input_parse(input_path)
+	output_data = output_parse(output_path)
+	real_input_data = np.delete(input_data, 0, axis=1)
+	real_output_data = np.delete(output_data, 0, axis=1)
+	feature_size = real_input_data.shape[1]
+	num_target = 3
+	input_index = 0
+	try:
+		for i in range(0, output_data.shape[0], batch_size):
+			if i + sequence_length + batch_size >= output_data.shape[0]:
+				# effective_batch_size = output_data.shape[0] - sequence_length - i
+				effective_batch_size = -1
+			else:
+				effective_batch_size = batch_size
+			if effective_batch_size <= 0:
+				X, y = None, None
+			else:
+				X = np.zeros(
+					(effective_batch_size, sequence_length, feature_size),
+					dtype=np.float32
+				)
+				y = np.zeros((effective_batch_size, num_target), dtype=np.float32) ####??????
+			if X is not None and y is not None:
+				for j in range(effective_batch_size):
+					pid = output_data[i + j][0]
+					start_index = input_index
+					if input_data[input_index][0] == pid:
+						while input_index < input_data.shape[0] and input_data[input_index][0] == pid:
+							input_index += 1
+						end_index = input_index
+						X[j, np.arange(end_index-start_index)] = real_input_data[start_index:end_index]
+						y[j] = real_output_data[i + j, :3]
+					else:
+						input_index += 1
+			yield X, y
+	except:
+		print('sequences generation is stopped!')
+	    
+	    
+	# finally:
+		# print('sequences generated successfully!')
+
+def test_sequences():
+	input_path = '../input_train.csv'
+	output_path = '../output_train.csv'
+	max_sequence_length = max_length(input_path)
+	max_sequence_length = 15
+	batch_size = 20
+	seq_iter = sequences(batch_size, max_sequence_length, input_path, output_path)
+	for (X, y) in seq_iter:
+		if X is not None and y is not None:
+			for i in range(X.shape[0]):
+				if i == 0:
+					print('X = %s, y = %s' % (
+						X[i], y[i]
+					))
+
+
+if __name__ == '__main__':
+	test_sequences()
+	
+
